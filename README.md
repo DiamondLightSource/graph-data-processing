66ac215d6957337d03949841c40350b008fb76de
iamvigneshwars <iamvigneshwars@gmail.com>
2024-03-26 15:49:07 +0000

    Create download link for processed images from s3 bucket'

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Dataset Service
+# DatasetProcessing Service
 
-A component of the `data-gateway` [federation](https://github.com/DiamondLightSource/data-gateway), exposing information from ISPyB about data collected during the Sessions (a.k.a. Visits) via GraphQL.
+A component of the `data-gateway` [federation](https://github.com/DiamondLightSource/data-gateway), exposing information from ISPyB about processed data collected during the Sessions (a.k.a. Visits) via GraphQL.
--- graph-data-processing/src/graphql/entities.rs
+++ graph-data-processing/src/graphql/entities.rs
@@ -2,20 +2,27 @@ use async_graphql::SimpleObject;
 use models::data_collection_file_attachment;
 
 #[derive(Clone, Debug, PartialEq, SimpleObject)]
-#[graphql(name = "DataProcessing", unresolvable)]
+#[graphql(name = "DataProcessing", unresolvable, complex)]
 pub struct DataProcessing {
     /// An opaque unique identifier for the collected file attachment
-    pub data_collection_file_attachment_id: u32,
+    pub id: u32,
     /// Full path where the processed image is stored
+    #[graphql(skip)]
     pub file_full_path: String,
 }
 
 impl From<data_collection_file_attachment::Model> for DataProcessing {
     fn from(values: data_collection_file_attachment::Model) -> Self {
         Self {
-            data_collection_file_attachment_id: values.data_collection_file_attachment_id,
+            id: values.data_collection_file_attachment_id,
             file_full_path: values.file_full_path,
         }
+    }
+}
+
+impl DataProcessing {
+    pub fn object_key(&self) -> String {
+        format!("{}", self.file_full_path)
     }
 }
 
